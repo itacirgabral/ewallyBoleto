4f056b8c1fbdbe85cabbb30d3f677052477fa068
Itacir Gabral <itacirgabral@gmail.com>
2022-04-10 16:11:45 -0400

    feat use query as datastore

--- src/app.js
+++ src/app.js
@@ -9,14 +9,26 @@ const bolivatorP = util.promisify(bolivator(ptBr))
 const app = express()
 
 app.get('/boleto/:barcode', (req, res) => {
-  const boleto = db.find(el => el.barCode === req.params.barcode)
-  if (!boleto) {
-    res.status(404).end()
-  } else {
-    console.dir(boleto)
-    bolivatorP(boleto)
+  if (Object.keys(req.query).length > 0) {
+    bolivatorP({
+      barCode: req.params.barcode,
+      dac10: Number(req.query.dac10),
+      amount: req.query.amount,
+      expirationDate: req.query.expirationDate,
+      titulo: req.query.titulo,
+      pagamento: req.query.pagamento
+    })
       .then(message => res.status(200).end(message))
       .catch(err => res.status(403).end(err.message))
+  } else {
+    const boleto = db.find(el => el.barCode === req.params.barcode)
+    if (!boleto) {
+      res.status(404).end()
+    } else {
+      bolivatorP(boleto)
+        .then(message => res.status(200).end(message))
+        .catch(err => res.status(403).end(err.message))
+    }
   }
 })
 
