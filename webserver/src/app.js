30c7405e5177bef4947e420cc41729dec7f1b7ac
Itacir Gabral <itacirgabral@gmail.com>
2022-04-24 22:34:11 -0400

    Bolivator in action

--- webserver/src/app.js
+++ webserver/src/app.js
@@ -7,8 +7,26 @@ app.get('/boleto/:linhaDigitavel', (req, res) => {
   const linhaDigitavel = req.params.linhaDigitavel
   try {
     const boleto = new Bolivator(linhaDigitavel)
-    console.dir(boleto)
-    res.status(300).end(':)')
+    if (boleto.dvxOK && boleto.dvyOK && boleto.dvzOK && boleto.barCodeOK) {
+      const codigoBarras = boleto.barCode.map(String).join('')
+      const vencimento = boleto.goodFor.toLocaleDateString('pt-BR', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+      })
+      const valor = boleto.amount.toLocaleString('pt-BR', {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
+      })
+
+      res.status(200).json({
+        codigoBarras,
+        valor,
+        vencimento
+      })
+    } else {
+      res.status(300).end('algo nao ta certo')
+    }
   } catch (err) {
     console.error(err)
     res.status(400).end('bad parameter')
