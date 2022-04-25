708e7d3f0d5fd832ce618f246b7310b7e44170d7
Itacir Gabral <itacirgabral@gmail.com>
2022-04-25 08:16:59 -0400

    feat accumulative erros

--- webserver/src/app.js
+++ webserver/src/app.js
@@ -7,7 +7,22 @@ app.get('/boleto/:linhaDigitavel', (req, res) => {
   const linhaDigitavel = req.params.linhaDigitavel
   try {
     const boleto = new Bolivator(linhaDigitavel)
-    if (boleto.dvxOK && boleto.dvyOK && boleto.dvzOK && boleto.barCodeOK) {
+
+    const errorsMessages = []
+    if (boleto.dvxOK) {
+      errorsMessages.push('O primeiro campo possui algum erro')
+    }
+    if (boleto.dvyOK) {
+      errorsMessages.push('O segundo campo possui algum erro')
+    }
+    if (boleto.dvzOK) {
+      errorsMessages.push('O terceiro campo possui algum erro')
+    }
+    if (boleto.barCodeOK) {
+      errorsMessages.push('O código de barras possui algum erro')
+    }
+
+    if (errorsMessages.length === 0) {
       const codigoBarras = boleto.barCode.map(String).join('')
       const vencimento = boleto.goodFor.toLocaleDateString('pt-BR', {
         year: 'numeric',
@@ -25,9 +40,16 @@ app.get('/boleto/:linhaDigitavel', (req, res) => {
         vencimento
       })
     } else {
-      res.status(300).end('algo nao ta certo')
+      res.status(300).end(errorsMessages.join('\n'))
     }
   } catch (err) {
+    /* Quando a linha digitavel não possui apenas numeros ou quando o tamanho
+     * dela é diferente significa que a requisição foi feita não utilizando o
+     * nosso frontend.
+     *
+     * Por outro lado, erros no conteúdo da linha digitável podem acontecer por
+     * descuido do usuario mas no uso adequado da interface.
+     */
     console.error(err)
     res.status(400).end('bad parameter')
   }
