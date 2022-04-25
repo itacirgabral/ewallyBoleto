ee8b87d98c0e13d01f1d02eb65fc32e44527d447
Itacir Gabral <itacirgabral@gmail.com>
2022-04-24 21:13:39 -0400

    local import

--- webserver/src/app.js
+++ webserver/src/app.js
@@ -1,10 +1,18 @@
 const express = require('express')
+const Bolivator = require('bolivator').default
 
 const app = express()
 
 app.get('/boleto/:linhaDigitavel', (req, res) => {
   const linhaDigitavel = req.params.linhaDigitavel
-  res.status(200).end(linhaDigitavel)
+  try {
+    const boleto = new Bolivator(linhaDigitavel)
+    console.dir(boleto)
+    res.status(300).end(':)')
+  } catch (err) {
+    console.error(err)
+    res.status(400).end('bad parameter')
+  }
 })
 
 app.listen(8080, () => {
