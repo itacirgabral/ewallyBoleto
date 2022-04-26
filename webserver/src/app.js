cc85d8328ddc29a1721bd7049510aff25703a239
Itacir Gabral <itacirgabral@gmail.com>
2022-04-26 14:05:08 -0400

    feat titulo lib

--- webserver/src/app.js
+++ webserver/src/app.js
@@ -1,56 +1,94 @@
 const express = require('express')
 const Bolivator = require('bolivator').default
+const Tituvator = require('tituvator').default
 
 const app = express()
 
 app.get('/boleto/:linhaDigitavel', (req, res) => {
   const linhaDigitavel = req.params.linhaDigitavel
-  try {
-    const boleto = new Bolivator(linhaDigitavel)
 
-    const errorsMessages = []
-    if (!boleto.dvxOK) {
-      errorsMessages.push('O primeiro campo possui algum erro')
-    }
-    if (!boleto.dvyOK) {
-      errorsMessages.push('O segundo campo possui algum erro')
-    }
-    if (!boleto.dvzOK) {
-      errorsMessages.push('O terceiro campo possui algum erro')
-    }
-    if (!boleto.barCodeOK) {
-      errorsMessages.push('O código de barras possui algum erro')
-    }
+  if (linhaDigitavel.length === 47) {
+    try {
+      const boleto = new Bolivator(linhaDigitavel)
 
-    if (errorsMessages.length === 0) {
-      const codigoBarras = boleto.barCode.map(String).join('')
-      const vencimento = boleto.goodFor.toLocaleDateString('pt-BR', {
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric'
-      })
-      const valor = boleto.amount.toLocaleString('pt-BR', {
-        minimumFractionDigits: 2,
-        maximumFractionDigits: 2
-      })
+      const errorsMessages = []
+      if (!boleto.dvxOK) {
+        errorsMessages.push('O primeiro campo possui algum erro')
+      }
+      if (!boleto.dvyOK) {
+        errorsMessages.push('O segundo campo possui algum erro')
+      }
+      if (!boleto.dvzOK) {
+        errorsMessages.push('O terceiro campo possui algum erro')
+      }
+      if (!boleto.barCodeOK) {
+        errorsMessages.push('O código de barras possui algum erro')
+      }
 
-      res.status(200).json({
-        codigoBarras,
-        valor,
-        vencimento
-      })
-    } else {
-      res.status(400).end(errorsMessages.join('\n'))
+      if (errorsMessages.length === 0) {
+        const codigoBarras = boleto.barCode.map(String).join('')
+        const vencimento = boleto.goodFor.toLocaleDateString('pt-BR', {
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric'
+        })
+        const valor = boleto.amount.toLocaleString('pt-BR', {
+          minimumFractionDigits: 2,
+          maximumFractionDigits: 2
+        })
+
+        res.status(200).json({
+          codigoBarras,
+          valor,
+          vencimento
+        })
+      } else {
+        res.status(400).end(errorsMessages.join('\n'))
+      }
+    } catch (err) {
+      console.error(err)
+      res.status(400).end('bad parameter')
     }
-  } catch (err) {
-    /* Quando a linha digitavel não possui apenas numeros ou quando o tamanho
-     * dela é diferente significa que a requisição foi feita não utilizando o
-     * nosso frontend.
-     *
-     * Por outro lado, erros no conteúdo da linha digitável podem acontecer por
-     * descuido do usuario mas no uso adequado da interface.
-     */
-    console.error(err)
+  } else if (linhaDigitavel.length === 48) {
+    try {
+      const titulo = new Tituvator(linhaDigitavel)
+      const errorsMessages = []
+
+      if (!titulo.dvaOK) {
+        errorsMessages.push('O primeiro campo possui algum erro')
+      }
+      if (!titulo.dvbOK) {
+        errorsMessages.push('O segundo campo possui algum erro')
+      }
+      if (!titulo.dvcOK) {
+        errorsMessages.push('O terceiro campo possui algum erro')
+      }
+      if (!titulo.dvcOK) {
+        errorsMessages.push('O terceiro campo possui algum erro')
+      }
+      if (!titulo.barCodeOK) {
+        errorsMessages.push('O código de barras possui algum erro')
+      }
+
+      if (errorsMessages.length === 0) {
+        const codigoBarras = titulo.barCode.map(String).join('')
+        const valor = titulo.amount.toLocaleString('pt-BR', {
+          minimumFractionDigits: 2,
+          maximumFractionDigits: 2
+        })
+
+        res.status(200).json({
+          codigoBarras,
+          valor
+        })
+      } else {
+        res.status(400).end(errorsMessages.join('\n'))
+      }
+    } catch (err) {
+      console.error(err)
+      res.status(400).end('bad parameter')
+    }
+  } else {
     res.status(400).end('bad parameter')
   }
 })
